5241f0f362867b916723c336415054e333a290c7
joemphilips <joemphilips@gmail.com>
2016-07-24 17:37:19 +0900

    mostly done but not using class

--- Fetcher/functions.py
+++ Fetcher/functions.py
@@ -9,35 +9,46 @@ logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler()
 logger.setLevel(logging.DEBUG)
 stream_handler.setLevel(logging.INFO)
-file_handler = logging.FileHandler(filename = 'log.txt')
+file_handler = logging.FileHandler(filename='log.txt')
 file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
+PAGES = set()
 
-def extract_link(url):
+
+def extract_link(url, degree=2):
     """特定のhtmlから全てのリンクを取得する
 
     Args:
         url (str): 取得したいhtmlのurl
     Yields:
-        str: リンク先のurl
+        set (str): リンク先のurl
     """
+    global PAGES
     result = requests.get(url)
     html = result.text
     bsObj = BeautifulSoup(html)
     wiki_pattern = re.compile("^(/wiki/)((?!:).)*$")
     body_content = bsObj.find("div", {"id": "bodyContent"})
     logger.debug("body content is {} ".format(body_content))
+
     for link in body_content.findAll("a",  href=wiki_pattern):
         if 'href' in link.attrs:
-            yield link.attrs['href']
+            if link.attrs['href'] not in PAGES:
+                newPage = link.attrs['href']
+                logger.info("found new page {} !!".format(newPage))
+                PAGES.add(newPage)
+                extract_link(newPage)
 
 
-def crowl(url, degree=2):
-    """urlからdegree内のリンクを全てたどりそのhtmlをダウンロードする。"""
-    for link in extract_link(url):
-        degree -= 1
+# def crowl(url, degree=2):
+#    """urlからdegree内のリンクを全てたどりそのhtmlをダウンロードする。"""
+#    urls = []
+#    for link in extract_link(url):
+#        degree -= 1
+#        urls.append(link)
+#        extrac_link(link)
 
 
 
