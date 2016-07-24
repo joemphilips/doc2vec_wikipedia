95cf2065f8ba5ffdf7e38f511c72f85193d6b8f2
joemphilips <joemphilips@gmail.com>
2016-07-24 22:18:39 +0900

    working with degree 1

--- Fetcher/functions.py
+++ Fetcher/functions.py
@@ -16,46 +16,18 @@ logger.addHandler(stream_handler)
 
 from collections import namedtuple
 
-url_with_degree = namedtuple("url_with_degree", ["url", "degree"])
+pageinfo = namedtuple("pageinfo", ["url", "degree", "body"])
 
-
-class LinkExtracter:
-
-    def __init__(self, url, degree=2):
-        self.first_url = pages_with_degree(url, degree)
-        self.all_urls = set()
-
-    def __call__(self):
-        self.extract_link(self.first_url)
-
-    def extract_link(self, url):
-        """特定のhtmlから全てのリンクを取得する
-
-        Args:
-            url (str): 取得したいhtmlのurl
-        Yields:
-            set (str): リンク先のurl
-        """
-        result = requests.get(url)
-        html = result.text
-        bsObj = BeautifulSoup(html)
-        wiki_pattern = re.compile("^(/wiki/)((?!:).)*$")
-        body_content = bsObj.find("div", {"id": "bodyContent"})
-        logger.debug("body content is {} ".format(body_content))
-
-        for link in body_content.findAll("a",  href=wiki_pattern):
-            if 'href' in link.attrs:
-                if link.attrs['href'] not in self.all_urls:
-                    newPage = link.attrs['href']
-                    logger.info("found new page {} !!".format(newPage))
-                    PAGES.add(newPage)
-                    extract_link(newPage)
-
-URLS = set()
+INFOS = set()
 WIKI_PATTERN = re.compile("^(/wiki/)((?!:).)*$")
+URLS = set()
 
 
-def extract_link(url, degree=2):
+def _extractcontent(html):
+    return html.get_text().replace(' ', '').replace("\n", '').replace("　", '')
+
+
+def crowl(url, degree=2):
     """特定のhtmlから全てのリンクを取得する
 
     Args:
@@ -63,39 +35,36 @@ def extract_link(url, degree=2):
     Yields:
         set (str): リンク先のurl
     """
-    global URLS
+    global URLS, INFOS
 
     result = requests.get(url)
     html = result.text
 
-    print("\t".join(url, degree, html))
-    URLS.add(url_with_degree(url, degree))
-    degree -= 1
-
+    print("\t".join([url, str(degree), html]))
     bsObj = BeautifulSoup(html)
     body_content = bsObj.find("div", {"id": "bodyContent"})
-    logger.debug("body content is {} ".format(body_content))
+    logger.debug("body content is {} and class it's class is {}".
+                 format(body_content, body_content.__class__))
+
+    content = _extractcontent(body_content)
+    INFOS.add(pageinfo(url, degree, content))
+
+    if degree == 0:
+        return
+    degree -= 1
 
     for link in body_content.findAll("a",  href=WIKI_PATTERN):
         if 'href' in link.attrs:
-            if link.attrs['href'] not in [u for u, _ in URLS]:
-                newPage = link.attrs['href']
-                logger.info("found new page {} !!".format(newPage))
-                URLS.add(url_with_degree(newPage, degree - 1)
-                extract_link(newPage, degree=degree)
-
-
-# def crowl(url, degree=2):
-#    """urlからdegree内のリンクを全てたどりそのhtmlをダウンロードする。"""
-#    urls = []
-#    for link in extract_link(url):
-#        degree -= 1
-#        urls.append(link)
-#        extrac_link(link)
-
-
+            if link.attrs['href'] not in URLS:
+                new_url = link.attrs['href']
+                logger.info("found new page {} !!".format(new_url))
+                URLS.add(new_url)
+                crowl("http://ja.wikipedia.org".format(new_url),
+                             degree=degree)
 
 
 if __name__ == '__main__':
-    extract_link("http://en.wikipedia.org/wiki/Pok%C3%A9mon_Go")
-
+    crowl("https://ja.wikipedia.org/wiki/Pok%C3%A9mon_GO", degree=1)
+    with open("result.csv", "w") as fh:
+        for info in INFOS:
+            fh.write(",".join([info.url, str(info.degree), info.body, "\n"]))
