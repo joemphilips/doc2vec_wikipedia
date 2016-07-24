20c9e8a71f360b722143629783c8a5a49d79af6b
joemphilips <joemphilips@gmail.com>
2016-07-24 18:18:03 +0900

    WIP:

--- Fetcher/functions.py
+++ Fetcher/functions.py
@@ -14,7 +14,45 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-PAGES = set()
+from collections import namedtuple
+
+url_with_degree = namedtuple("url_with_degree", ["url", "degree"])
+
+
+class LinkExtracter:
+
+    def __init__(self, url, degree=2):
+        self.first_url = pages_with_degree(url, degree)
+        self.all_urls = set()
+
+    def __call__(self):
+        self.extract_link(self.first_url)
+
+    def extract_link(self, url):
+        """特定のhtmlから全てのリンクを取得する
+
+        Args:
+            url (str): 取得したいhtmlのurl
+        Yields:
+            set (str): リンク先のurl
+        """
+        result = requests.get(url)
+        html = result.text
+        bsObj = BeautifulSoup(html)
+        wiki_pattern = re.compile("^(/wiki/)((?!:).)*$")
+        body_content = bsObj.find("div", {"id": "bodyContent"})
+        logger.debug("body content is {} ".format(body_content))
+
+        for link in body_content.findAll("a",  href=wiki_pattern):
+            if 'href' in link.attrs:
+                if link.attrs['href'] not in self.all_urls:
+                    newPage = link.attrs['href']
+                    logger.info("found new page {} !!".format(newPage))
+                    PAGES.add(newPage)
+                    extract_link(newPage)
+
+URLS = set()
+WIKI_PATTERN = re.compile("^(/wiki/)((?!:).)*$")
 
 
 def extract_link(url, degree=2):
@@ -25,21 +63,26 @@ def extract_link(url, degree=2):
     Yields:
         set (str): リンク先のurl
     """
-    global PAGES
+    global URLS
+
     result = requests.get(url)
     html = result.text
+
+    print("\t".join(url, degree, html))
+    URLS.add(url_with_degree(url, degree))
+    degree -= 1
+
     bsObj = BeautifulSoup(html)
-    wiki_pattern = re.compile("^(/wiki/)((?!:).)*$")
     body_content = bsObj.find("div", {"id": "bodyContent"})
     logger.debug("body content is {} ".format(body_content))
 
-    for link in body_content.findAll("a",  href=wiki_pattern):
+    for link in body_content.findAll("a",  href=WIKI_PATTERN):
         if 'href' in link.attrs:
-            if link.attrs['href'] not in PAGES:
+            if link.attrs['href'] not in [u for u, _ in URLS]:
                 newPage = link.attrs['href']
                 logger.info("found new page {} !!".format(newPage))
-                PAGES.add(newPage)
-                extract_link(newPage)
+                URLS.add(url_with_degree(newPage, degree - 1)
+                extract_link(newPage, degree=degree)
 
 
 # def crowl(url, degree=2):
