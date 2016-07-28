0c604f69721922241c8866f64140194fd1bca1b2
joemphilips <joemphilips@gmail.com>
2016-07-28 18:19:28 +0000

    output grpah matrix

--- Fetcher/graph_matrix.py
+++ Fetcher/graph_matrix.py
@@ -3,6 +3,7 @@
 from glob import glob
 from Fetcher.functions import extract_body, WIKI_PATTERN
 from collections import OrderedDict
+import csv
 import logging
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s --- %(filename)s --- %(levelname)s --- %(message)s")
@@ -19,7 +20,7 @@ def get_all_link_list(path):
     return [ "/wiki/" + l.split("/")[-1].split('.')[0] for l in  glob(path + '/*.html')]
 
 
-def graph_matrix(html, all_link):
+def examine_haslink(html, all_link):
     """
     Args:
         html (str): full html text
@@ -35,18 +36,22 @@ def graph_matrix(html, all_link):
     links =  [l.attrs['href'] for l in body_content.findAll("a", href=WIKI_PATTERN) if 'href' in l.attrs]
     logger.debug("all_link are {} and links in documents are {}".format(all_link, links))
     for l in all_link:
-        has_link_dict[l] = True if l in links else False
+        has_link_dict[l] = 1 if l in links else 0
     return has_link_dict
 
 
+def graph_matrix(html_file_dir="/mnt/ebs/tmp", outputfile="graph.csv"):
+    all_link = get_all_link_list(html_file_dir)
+    html_file_paths = glob("/mnt/ebs/tmp" + '/*html')
+    with open(outputfile, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = all_link)
+        writer.writeheader()
+        for i, html_path in enumerate(html_file_paths):
+            with open(html_path) as fh:
+                html = fh.read()
+            has_link_or_not = examine_haslink(html, all_link)
+            writer.writerow(has_link_or_not)
+
 
 if __name__ == '__main__':
-    all_link = get_all_link_list("/mnt/ebs/tmp")
-    for i, html_file_path in enumerate(glob("/mnt/ebs/tmp" + '/*html')):
-        html_id = html_file_path.split("/")[-1].split('.')[0]
-        with open(html_file_path) as fh:
-            html = fh.read()
-        has_link_or_not = graph_matrix(html, all_link)
-        if i == 0:
-            print("#id", ",", ",".join(['#' + str(k) for k in has_link_or_not.keys()])) # header
-        print(html_id, ",", ",".join([ str(int(b)) for b in has_link_or_not.values()]))
+    graph_matrix()
